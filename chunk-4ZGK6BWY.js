e8de5d8dcd9a126418ba831fa4585d0480844e2e
Eduardo-Cenci <eduardocenci31@gmail.com>
2024-02-23 00:28:28 -0300

    update

---
+++ chunk-4ZGK6BWY.js
@@ -0,0 +1,53 @@
+import{A as P,B as T,C as c,D as m,E as _,F as w,G as y,K as x,O as D,d as g,f as v,g as M,j as O,k as u,l as I,m as a,n as k,o as S,p,q as E,r as F,s as h,t as f,u as b,v as r,w as s,x as l,y as z,z as C}from"./chunk-7D4FNC7N.js";var B=(()=>{let e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=g({type:e,selectors:[["app-dish-card"]],inputs:{dish:"dish"},standalone:!0,features:[_],decls:10,vars:7,consts:[[1,"card"],[1,"card__img",3,"src","alt"],[1,"card__content"],[1,"card__heading"],[1,"card__text"],[1,"card__price"]],template:function(t,i){t&1&&(r(0,"article",0),l(1,"img",1),r(2,"div",2)(3,"h3",3),c(4),s(),r(5,"p",4),c(6),s(),r(7,"strong",5),c(8),w(9,"currency"),s()()()),t&2&&(a(),p("src",i.dish.img,u)("alt",i.dish.name),a(3),m(" ",i.dish.name," "),a(2),m(" ",i.dish.description," "),a(2),m(" ",y(9,5,i.dish.price)," "))},dependencies:[x],styles:[".card[_ngcontent-%COMP%]{border-radius:1rem;overflow:hidden;cursor:pointer;box-shadow:#26443f40 0 0 1rem;transition:box-shadow .25s ease-in-out}.card[_ngcontent-%COMP%]:hover{box-shadow:#26443f80 0 0 1rem}.card__img[_ngcontent-%COMP%]{display:block;width:100%;height:10rem;object-fit:cover;background-color:#d67e24}.card__content[_ngcontent-%COMP%]{display:flex;flex-direction:column;gap:1rem;padding:1.5rem}.card__heading[_ngcontent-%COMP%]{font-weight:600}.card__text[_ngcontent-%COMP%]{height:2.5rem;overflow:hidden;font-size:.9rem;line-height:1.25rem;white-space:pre-line;color:#38655d}.card__price[_ngcontent-%COMP%]{font-weight:700}"]});let n=e;return n})();var H=[{id:1,name:"Vegetariano"},{id:2,name:"Saud\xE1vel"},{id:3,name:"Gourmet"},{id:4,name:"Lanches"}];var A=[{id:1,categoryId:1,name:"Cogumelos refogados com ricota",description:`Cogumelos Refogados com ricota na acelga, arroz com br\xF3colis e feij\xE3o vermelho. 
+
+Peso: 350g a 400g`,price:29.9,img:"assets/imgs/prato.webp"},{id:2,categoryId:1,name:"Feijoada vegetariana",description:`Feijoada vegetariana, couve refogada, arroz integral e laranja. 
+
+Peso: 350g a 400g`,price:29.9,img:"assets/imgs/prato.webp"},{id:3,categoryId:2,name:"Sobrecoxa assada",description:`Sobrecoxa assada, cubinhos de batata doce e legumes salteados. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:4,categoryId:2,name:"Cubos de carne com batatas ao molho",description:`Cubos de carne com batatas ao molho, arroz integral, feij\xE3o carioca e farofa de banana. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:5,categoryId:2,name:"Salm\xE3o grelhado",description:`Salm\xE3o grelhado com creme de beterraba e gr\xE3o de bico. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:6,categoryId:2,name:"Til\xE1pia grelhada",description:`Til\xE1pia grelhada com legumes e castanhas. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:7,categoryId:2,name:"Cubos de sobrecoxa grelhados",description:`Cubos de sobrecoxa grelhados, arroz, feij\xE3o vermelho e legumes salteados. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:8,categoryId:3,name:"Espaguete com alm\xF4ndegas",description:`Espaguete ao molho vermelho com alm\xF4ndegas. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:9,categoryId:3,name:"Frango xadrez",description:`Frango xadrez com arroz e castanhas. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:10,categoryId:3,name:"Strogonoff de mignon",description:`Strogonoff de mignon, arroz e batata sout\xE9. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:11,categoryId:3,name:"Strogonoff de frango",description:`Strogonoff de frango, arroz e batata sout\xE9. 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:12,categoryId:4,name:"Crepioca",description:`Crepioca de costela desfiada e/ou carne seca 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:13,categoryId:4,name:"Panqueca",description:`Panqueca de costela desfiada e/ou carne seca 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"},{id:14,categoryId:4,name:"Escodidinho",description:`Escondidinho de costela desfiada e/ou carne seca 
+
+Peso: 350g a 400g 
+
+Prote\xEDna: 150g`,price:29.9,img:"assets/imgs/prato.webp"}];var L=(()=>{let e=class e{constructor(o){this.host=o,this.isActive=!1}open(){this.isActive=!0}close(o){let t=this.host.nativeElement.querySelector("article");(this.isActive&&!o||this.isActive&&o&&!t.contains(o))&&(this.isActive=!1)}};e.\u0275fac=function(t){return new(t||e)(k(I))},e.\u0275cmp=g({type:e,selectors:[["app-dish-modal"]],hostVars:2,hostBindings:function(t,i){t&1&&C("keydown.escape",function(){return i.close()},!1,O)("click",function($){return i.close($.target)}),t&2&&E("active",i.isActive)},inputs:{dish:"dish"},standalone:!0,features:[_],decls:10,vars:7,consts:[[1,"modal"],[1,"modal__img",3,"src","alt"],[1,"modal__content"],[1,"modal__heading"],[1,"modal__text"],[1,"modal__price"]],template:function(t,i){t&1&&(r(0,"article",0),l(1,"img",1),r(2,"div",2)(3,"h4",3),c(4),s(),r(5,"p",4),c(6),s(),r(7,"strong",5),c(8),w(9,"currency"),s()()()),t&2&&(a(),p("src",i.dish.img,u)("alt",i.dish.name),a(3),m(" ",i.dish.name," "),a(2),m(" ",i.dish.description," "),a(2),m(" ",y(9,5,i.dish.price)," "))},dependencies:[x],styles:["[_nghost-%COMP%]{position:fixed;z-index:2;inset:0;-webkit-backdrop-filter:blur(.25rem) brightness(.5);backdrop-filter:blur(.25rem) brightness(.5);background:radial-gradient(circle,#00000080,#0000);display:flex;align-items:center;justify-content:center;padding:1.5rem;opacity:0;visibility:hidden;transition:all .25s ease-in-out}.active[_nghost-%COMP%]{opacity:1;visibility:visible}.modal[_ngcontent-%COMP%]{width:100%;max-width:50rem;max-height:100%;border-radius:1rem;overflow:auto;background-color:#fff;box-shadow:#26443f40 0 0 1rem}.modal__img[_ngcontent-%COMP%]{display:block;width:100%;height:25rem;object-fit:cover;background-color:#d67e24}.modal__content[_ngcontent-%COMP%]{display:flex;flex-direction:column;gap:1.5rem;padding:1.5rem}.modal__heading[_ngcontent-%COMP%]{font-weight:600}.modal__text[_ngcontent-%COMP%]{font-size:.9rem;line-height:1.25rem;white-space:pre-line;color:#38655d}.modal__price[_ngcontent-%COMP%]{font-weight:700}"]});let n=e;return n})();function q(n,e){if(n&1&&(r(0,"a",3),c(1),s()),n&2){let d=e.$implicit;p("fragment",d.name),a(),m(" ",d.name," ")}}function R(n,e){if(n&1){let d=z();r(0,"li",8)(1,"app-dish-card",9),C("click",function(){v(d);let t=T(3);return M(t.open())}),s(),l(2,"app-dish-modal",10,11),s()}if(n&2){let d=P().$implicit;a(),p("dish",d),a(),p("dish",d)}}function U(n,e){if(n&1&&S(0,R,4,2,"li",7),n&2){let d=e.$implicit,o=P().$implicit;F(0,d.categoryId===o.id?0:-1)}}function G(n,e){if(n&1&&(r(0,"section",4)(1,"h2",5),c(2),s(),r(3,"ul",6),f(4,U,1,1,null,null,h),s()()),n&2){let d=e.$implicit,o=P();p("id",d.name),a(2),m(" ",d.name," "),a(2),b(o.dishes)}}var re=(()=>{let e=class e{constructor(){this.categories=H,this.dishes=A}};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=g({type:e,selectors:[["app-menu"]],standalone:!0,features:[_],decls:7,vars:0,consts:[[1,"menu"],[1,"menu__categories"],[1,"menu__categories__wrapper"],["routerLink","/cardapio",1,"menu__category",3,"fragment"],[1,"menu__section",3,"id"],[1,"menu__heading"],[1,"menu__list"],["class","menu__item"],[1,"menu__item"],[3,"dish","click"],[3,"dish"],["dishModal",""],["class","menu__category","routerLink","/cardapio",3,"fragment"],["class","menu__section",3,"id"]],template:function(t,i){t&1&&(r(0,"main",0)(1,"nav",1)(2,"div",2),f(3,q,2,2,"a",12,h),s()(),f(5,G,6,2,"section",13,h),s()),t&2&&(a(3),b(i.categories),a(2),b(i.categories))},dependencies:[B,L,D],styles:[".menu[_ngcontent-%COMP%]{padding-top:3rem!important;padding:1.5rem}@media screen and (min-width: 600px){.menu[_ngcontent-%COMP%]{padding:2rem}}@media screen and (min-width: 900px){.menu[_ngcontent-%COMP%]{padding:2.5rem}}@media screen and (min-width: 1200px){.menu[_ngcontent-%COMP%]{padding:3rem}}@media screen and (min-width: 1200px){.menu[_ngcontent-%COMP%]{padding:3.5rem}}@media screen and (min-width: 1500px){.menu[_ngcontent-%COMP%]{padding:4rem}}.menu__categories[_ngcontent-%COMP%]{position:fixed;z-index:1;inset:5rem 0 auto;height:3rem;overflow:hidden}@media screen and (min-width: 900px){.menu__categories[_ngcontent-%COMP%]{top:6rem}}.menu__categories__wrapper[_ngcontent-%COMP%]{overflow-x:auto;display:flex;gap:.5rem;align-items:center;-webkit-backdrop-filter:blur(.25rem) brightness(.75);backdrop-filter:blur(.25rem) brightness(.75);padding-top:.5rem!important;padding-bottom:.5rem!important;padding:1.5rem}@media screen and (min-width: 600px){.menu__categories__wrapper[_ngcontent-%COMP%]{padding:2rem}}@media screen and (min-width: 900px){.menu__categories__wrapper[_ngcontent-%COMP%]{padding:2.5rem}}@media screen and (min-width: 1200px){.menu__categories__wrapper[_ngcontent-%COMP%]{padding:3rem}}@media screen and (min-width: 1200px){.menu__categories__wrapper[_ngcontent-%COMP%]{padding:3.5rem}}@media screen and (min-width: 1500px){.menu__categories__wrapper[_ngcontent-%COMP%]{padding:4rem}}.menu__category[_ngcontent-%COMP%]{border-radius:1.5rem;padding:.5rem 1.5rem;line-height:1rem;color:#fff;background-color:#26443f;transition:all .25s ease-in-out}.menu__category[_ngcontent-%COMP%]:hover{color:#26443f;background-color:#fff;box-shadow:#26443f40 0 0 1rem}.menu__section[_ngcontent-%COMP%]{padding-top:2rem}.menu__heading[_ngcontent-%COMP%]{font-size:1.5rem;font-weight:600;line-height:1.5;margin-bottom:2rem;color:#d67e24}.menu__list[_ngcontent-%COMP%]{display:grid;grid-template-columns:repeat(auto-fill,minmax(min(25rem,100%),1fr));gap:2rem}"]});let n=e;return n})();export{re as default};
